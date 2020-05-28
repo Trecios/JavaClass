a43726e3fc268d724b43b3c58599c23ec69fab92
felip <felip@Trecios>
2020-05-28 15:02:00 -0300

    Unidade 3 - Aula 41

---
+++ cursoAlgoritmosProgramacao/src/unidade2/HelloWorld.java
@@ -0,0 +1,13 @@
+package unidade2;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste");
+		JOptionPane.showMessageDialog(null, "Teste");
+	}
+
+}
